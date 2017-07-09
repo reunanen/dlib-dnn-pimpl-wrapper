05e1acc62b7cb68bbe8195a39fd9704a928636c0
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-07-09 11:39:56 +0300

    Widen the autoencoder

--- NetStructure.h
+++ NetStructure.h
@@ -30,24 +30,24 @@ template <int N, typename SUBNET> using ares_up = dlib::relu<residual_up<block, 
 
 // ----------------------------------------------------------------------------------------
 
-template <typename SUBNET> using level1 = res<8, /*res<8,*/ res_down<8, SUBNET>/*>*/>;
-template <typename SUBNET> using level2 = res<8, /*res<8, res<8, res<8, res<8,*/ res_down<8, SUBNET>/*>>>>*/>;
-template <typename SUBNET> using level3 = res<8, /*res<8, res<8,*/ res_down<8, SUBNET>/*>>*/>;
+template <typename SUBNET> using level1 = res<64, /*res<8,*/ res_down<64, SUBNET>/*>*/>;
+template <typename SUBNET> using level2 = res<32, /*res<8, res<8, res<8, res<8,*/ res_down<32, SUBNET>/*>>>>*/>;
+template <typename SUBNET> using level3 = res<16, /*res<8, res<8,*/ res_down<16, SUBNET>/*>>*/>;
 template <typename SUBNET> using level4 = res<8, /*res<8,*/ res_down<8, SUBNET>/*>*/>;
 
-template <typename SUBNET> using alevel1 = ares<8, /*ares<8,*/ ares_down<8, SUBNET>/*>*/>;
-template <typename SUBNET> using alevel2 = ares<8, /*ares<8, ares<8, ares<8, ares<8,*/ ares_down<8, SUBNET>/*>>>>*/>;
-template <typename SUBNET> using alevel3 = ares<8, /*ares<8, ares<8,*/ ares_down<8, SUBNET>/*>>*/>;
+template <typename SUBNET> using alevel1 = ares<64, /*ares<8,*/ ares_down<64, SUBNET>/*>*/>;
+template <typename SUBNET> using alevel2 = ares<32, /*ares<8, ares<8, ares<8, ares<8,*/ ares_down<32, SUBNET>/*>>>>*/>;
+template <typename SUBNET> using alevel3 = ares<16, /*ares<8, ares<8,*/ ares_down<16, SUBNET>/*>>*/>;
 template <typename SUBNET> using alevel4 = ares<8, /*ares<8,*/ ares_down<8, SUBNET>/*>*/>;
 
-template <typename SUBNET> using level1t = res<8, /*res<8,*/ res_up<8, SUBNET>/*>*/>;
-template <typename SUBNET> using level2t = res<8, /*res<8, res<8, res<8, res<8,*/ res_up<8, SUBNET>/*>>>>*/>;
-template <typename SUBNET> using level3t = res<8, /*res<8, res<8,*/ res_up<8, SUBNET>/*>>*/>;
+template <typename SUBNET> using level1t = res<64, /*res<8,*/ res_up<64, SUBNET>/*>*/>;
+template <typename SUBNET> using level2t = res<32, /*res<8, res<8, res<8, res<8,*/ res_up<32, SUBNET>/*>>>>*/>;
+template <typename SUBNET> using level3t = res<16, /*res<8, res<8,*/ res_up<16, SUBNET>/*>>*/>;
 template <typename SUBNET> using level4t = res<8, /*res<8,*/ res_up<8, SUBNET>/*>*/>;
 
-template <typename SUBNET> using alevel1t = ares<8, /*ares<8,*/ ares_up<8, SUBNET>/*>*/>;
-template <typename SUBNET> using alevel2t = ares<8, /*ares<8, ares<8, ares<8, ares<8,*/ ares_up<8, SUBNET>/*>>>>*/>;
-template <typename SUBNET> using alevel3t = ares<8, /*ares<8, ares<8,*/ ares_up<8, SUBNET>/*>>*/>;
+template <typename SUBNET> using alevel1t = ares<64, /*ares<8,*/ ares_up<64, SUBNET>/*>*/>;
+template <typename SUBNET> using alevel2t = ares<32, /*ares<8, ares<8, ares<8, ares<8,*/ ares_up<32, SUBNET>/*>>>>*/>;
+template <typename SUBNET> using alevel3t = ares<16, /*ares<8, ares<8,*/ ares_up<16, SUBNET>/*>>*/>;
 template <typename SUBNET> using alevel4t = ares<8, /*ares<8,*/ ares_up<8, SUBNET>/*>*/>;
 
 // training network type
@@ -56,7 +56,7 @@ using net_type = dlib::loss_mean_squared_per_pixel<
     dlib::relu<dlib::bn_con<dlib::cont<8, 7, 7, 3, 3,
     level4t<level3t<level2t<level1t<
     level1<level2<level3<level4<
-    dlib::max_pool<7, 7, 3, 3, dlib::relu<dlib::bn_con<dlib::con<32, 7, 7, 3, 3,
+    dlib::max_pool<7, 7, 3, 3, dlib::relu<dlib::bn_con<dlib::con<8, 7, 7, 3, 3,
     dlib::input<dlib::matrix<float>>
     >>>>>>>>>>>>>>>>>>;
 
@@ -66,7 +66,7 @@ using anet_type = dlib::loss_mean_squared_per_pixel<
     dlib::relu<dlib::affine<dlib::cont<8, 7, 7, 3, 3,
     alevel4t<alevel3t<alevel2t<alevel1t<
     alevel1<alevel2<alevel3<alevel4<
-    dlib::max_pool<7, 7, 3, 3, dlib::relu<dlib::affine<dlib::con<32, 7, 7, 3, 3,
+    dlib::max_pool<7, 7, 3, 3, dlib::relu<dlib::affine<dlib::con<8, 7, 7, 3, 3,
     dlib::input<dlib::matrix<float>>
     >>>>>>>>>>>>>>>>>>;
 
