519499514c1ab8794e726272341c2b994eba798f
Juha Reunanen <juha.reunanen@tomaattinen.com>
2017-07-09 11:39:19 +0300

    Create separate TrainingNet and RuntimeNet classes

--- NetPimpl.cpp
+++ NetPimpl.cpp
@@ -1,91 +1,109 @@
 #include "NetPimpl.h"
 #include "NetStructure.h"
 
-struct NetPimpl::Impl
+namespace NetPimpl {
+
+struct TrainingNet::Impl
 {
     std::unique_ptr<net_type> net;
     std::unique_ptr<dlib::dnn_trainer<net_type>> trainer;
-
-    std::unique_ptr<net_type> cleanNet;
-    std::unique_ptr<anet_type> anet;
 };
 
-NetPimpl::NetPimpl()
+struct RuntimeNet::Impl
 {
-    pimpl = new NetPimpl::Impl();
+    anet_type anet;
+};
+
+TrainingNet::TrainingNet()
+{
+    pimpl = new TrainingNet::Impl();
 }
 
-NetPimpl::~NetPimpl()
+TrainingNet::~TrainingNet()
 {
     delete pimpl;
 }
 
-void NetPimpl::InitializeForTraining(const solver_type& solver)
+void TrainingNet::Initialize(const solver_type& solver)
 {
     pimpl->net = std::make_unique<net_type>();
     pimpl->trainer = std::make_unique<dlib::dnn_trainer<net_type>>(*pimpl->net, solver);
     pimpl->trainer->be_verbose(); // TODO: remove
 }
 
-void NetPimpl::SetLearningRate(double learningRate)
+void TrainingNet::SetLearningRate(double learningRate)
 {
     pimpl->trainer->set_learning_rate(learningRate);
 }
 
-void NetPimpl::SetMinLearningRate(double minLearningRate)
+void TrainingNet::SetMinLearningRate(double minLearningRate)
 {
     pimpl->trainer->set_min_learning_rate(minLearningRate);
 }
 
-void NetPimpl::SetIterationsWithoutProgressThreshold(unsigned long threshold)
+void TrainingNet::SetIterationsWithoutProgressThreshold(unsigned long threshold)
 {
     pimpl->trainer->set_iterations_without_progress_threshold(threshold);
 }
 
-void NetPimpl::SetSynchronizationFile(const std::string& filename, std::chrono::seconds time_between_syncs)
+void TrainingNet::SetSynchronizationFile(const std::string& filename, std::chrono::seconds time_between_syncs)
 {
     pimpl->trainer->set_synchronization_file(filename, time_between_syncs);
 }
 
-void NetPimpl::StartTraining(const std::vector<NetPimpl::input_type>& inputs, const std::vector<NetPimpl::training_label_type>& training_labels)
+void TrainingNet::StartTraining(const std::vector<input_type>& inputs, const std::vector<training_label_type>& training_labels)
 {
     pimpl->trainer->train_one_step(inputs, training_labels);
 }
 
-void NetPimpl::GetNet()
+RuntimeNet TrainingNet::GetRuntimeNet() const
 {
-    pimpl->trainer->get_net();
-    if (!pimpl->cleanNet.get()) {
-        pimpl->cleanNet = std::make_unique<net_type>();
-    }
-    *pimpl->cleanNet = *pimpl->net;
-    pimpl->cleanNet->clean();
-    if (!pimpl->anet.get()) {
-        pimpl->anet = std::make_unique<anet_type>();
-    }
-    *pimpl->anet = *pimpl->cleanNet;
+    RuntimeNet runtimeNet;
+    runtimeNet = *this; // there's only assignment operator - no copy constructor (at least for now)
+    return runtimeNet;
 }
 
-NetPimpl::output_type NetPimpl::operator() (const NetPimpl::input_type& input) const
+RuntimeNet::RuntimeNet()
 {
-    if (!pimpl->anet.get()) {
-        pimpl->anet = std::make_unique<anet_type>();
-    }
-    return (*pimpl->anet)(input);
+    pimpl = new RuntimeNet::Impl();
 }
 
-void NetPimpl::Serialize(std::ostream& out) const
+RuntimeNet::~RuntimeNet()
 {
-    if (!pimpl->anet.get()) {
-        pimpl->anet = std::make_unique<anet_type>();
-    }
-    dlib::serialize(*pimpl->anet, out);
+    delete pimpl;
 }
 
-void NetPimpl::Deserialize(std::istream& in) const
+RuntimeNet::RuntimeNet(const RuntimeNet& that)
 {
-    if (!pimpl->anet.get()) {
-        pimpl->anet = std::make_unique<anet_type>();
-    }
-    dlib::deserialize(*pimpl->anet, in);
+    pimpl = new RuntimeNet::Impl();
+    operator= (that);
+}
+
+RuntimeNet& RuntimeNet::operator= (const RuntimeNet& that)
+{
+    pimpl->anet = that.pimpl->anet;
+    return *this;
+}
+
+RuntimeNet& RuntimeNet::operator= (const TrainingNet& trainingNet)
+{
+    pimpl->anet = trainingNet.pimpl->trainer->get_net(); // may block
+    return *this;
+}
+
+output_type RuntimeNet::operator() (const input_type& input) const
+{
+    return pimpl->anet(input);
+}
+
+void RuntimeNet::Serialize(std::ostream& out) const
+{
+    dlib::serialize(pimpl->anet, out);
+}
+
+void RuntimeNet::Deserialize(std::istream& in) const
+{
+    dlib::deserialize(pimpl->anet, in);
+}
+
 }
--- NetPimpl.h
+++ NetPimpl.h
@@ -2,33 +2,58 @@
 
 #include <dlib/dnn.h>
 
-class NetPimpl
+namespace NetPimpl
 {
-public:
     typedef dlib::matrix<float> input_type;
     typedef dlib::matrix<float> training_label_type;
     typedef dlib::matrix<float> output_type;
     typedef dlib::sgd solver_type;
 
-    NetPimpl();
-    virtual ~NetPimpl();
+    class RuntimeNet;
 
-    void InitializeForTraining(const solver_type& solver = dlib::sgd(0.0001, 0.9));
+    class TrainingNet {
+    public:
+        TrainingNet();
+        virtual ~TrainingNet();
 
-    void SetLearningRate(double learningRate);
-    void SetMinLearningRate(double minLearningRate);
-    void SetIterationsWithoutProgressThreshold(unsigned long threshold);
-    void SetSynchronizationFile(const std::string& filename, std::chrono::seconds time_between_syncs = std::chrono::minutes(15));
+        void Initialize(const solver_type& solver = dlib::sgd(0.0001, 0.9));
 
-    void StartTraining(const std::vector<input_type>& inputs, const std::vector<training_label_type>& training_labels);
-    void GetNet();
+        void SetLearningRate(double learningRate);
+        void SetMinLearningRate(double minLearningRate);
+        void SetIterationsWithoutProgressThreshold(unsigned long threshold);
+        void SetSynchronizationFile(const std::string& filename, std::chrono::seconds time_between_syncs = std::chrono::minutes(15));
 
-    output_type operator() (const input_type& input) const;
+        void StartTraining(const std::vector<input_type>& inputs, const std::vector<training_label_type>& training_labels);
 
-    void Serialize(std::ostream& out) const;
-    void Deserialize(std::istream& in) const;
+        RuntimeNet GetRuntimeNet() const; // may block
 
-private:
-    struct Impl;
-    Impl* pimpl;
+    private:
+        TrainingNet(const TrainingNet&) = delete;
+        TrainingNet& operator= (const TrainingNet&) = delete;
+
+        struct Impl;
+        Impl* pimpl;
+
+        friend class RuntimeNet;
+    };
+
+    class RuntimeNet {
+    public:
+        RuntimeNet();
+        virtual ~RuntimeNet();
+
+        RuntimeNet(const RuntimeNet&);
+        RuntimeNet& operator= (const RuntimeNet&);
+
+        RuntimeNet& operator= (const TrainingNet& trainingNet); // may block
+
+        output_type operator() (const input_type& input) const;
+
+        void Serialize(std::ostream& out) const;
+        void Deserialize(std::istream& in) const;
+
+    private:
+        struct Impl;
+        Impl* pimpl;
+    };
 };
